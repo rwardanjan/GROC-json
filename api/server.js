8857c27c050c590eeddd2375eb4e5bcd2ddb6cc1
Rudolf <rud_olf@hotmail.com>
2024-07-21 20:42:50 +0200

    auth

--- api/server.js
+++ api/server.js
@@ -1,21 +1,34 @@
-// JSON Server module
+// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+const auth = require("json-server-auth");
+const cors = require("cors");
 
-// Make sure to use the default middleware
+const server = jsonServer.create();
+
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+const router = jsonServer.router("db.json");
+
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
 server.use(middlewares);
+server.use(auth);
+
 // Add this before server.use(router)
 server.use(
-  // Add custom route here if needed
   jsonServer.rewriter({
     "/api/*": "/$1",
   })
 );
 server.use(router);
-// Listen to port
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
