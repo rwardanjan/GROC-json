a14a03a2067d20d4922d30d0838e135bbf4524dd
Rudolf <rud_olf@hotmail.com>
2024-07-21 21:15:37 +0200

    removed auth

--- api/server.js
+++ api/server.js
@@ -1,7 +1,5 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
-const auth = require("json-server-auth");
-const cors = require("cors");
 
 const server = jsonServer.create();
 
@@ -18,14 +16,12 @@ const router = jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
 server.use(middlewares);
-server.use(auth);
-
 // Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
   })
 );
 server.use(router);
